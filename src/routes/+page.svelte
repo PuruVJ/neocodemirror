99bf7c5debb013ab33df751609a9fb067e1635ec
Puru Vijay <devpuruvj@gmail.com>
2023-04-27 00:34:20 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { codemirror } from '$lib';
+	import { javascript } from '@codemirror/lang-javascript';
+</script>
+
+<div
+	use:codemirror={{
+		value: 'const js = "bwahahahha"',
+		setup: 'basic',
+		lang: javascript({ typescript: true }),
+	}}
+/>
